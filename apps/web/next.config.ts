581748c91f76d053b9ffc98274df5c16af8b8130
Krishna Rokhale <krishna.rokhale@gmail.com>
2026-08-13 13:20:47 +0530

    updated nextjs version

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	devIndicators: false,
+	// devIndicators: false,
 	output: "standalone",
 	serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
 	reactCompiler: true,
